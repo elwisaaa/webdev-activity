d4f4d7852c565e1bd636b69a70786261b0f04221
elwisaaa <166204210+elwisaaa@users.noreply.github.com>
2024-10-29 10:16:14 +0800

    Add files via upload

---
+++ classes/account.class.php
@@ -0,0 +1,118 @@
+<?php
+
+require_once 'database.php';
+
+class Account{
+    public $id = '';
+    public $first_name = '';
+    public $last_name = '';
+    public $username = '';
+    public $password = '';
+    public $role = 'staff';
+    public $is_staff = true;
+    public $is_admin = false;
+
+
+    protected $db;
+
+    function __construct(){
+        $this->db = new Database();
+    }
+
+    function add(){
+        $sql = "INSERT INTO account (first_name, last_name, username, password, role, is_staff, is_admin) VALUES (:first_name, :last_name, :username, :password, :role, :is_staff, :is_admin);";
+        $query = $this->db->connect()->prepare($sql);
+
+        $query->bindParam(':first_name', $this->first_name);
+        $query->bindParam(':last_name', $this->last_name);
+        $query->bindParam(':username', $this->username);
+        $hashpassword = password_hash($this->password, PASSWORD_DEFAULT);
+        $query->bindParam(':password', $hashpassword);
+        $query->bindParam(':role', $this->role);
+        $query->bindParam(':is_staff', $this->is_staff);
+        $query->bindParam(':is_admin', $this->is_admin);
+
+        return $query->execute();
+    }
+
+    function usernameExist($username, $excludeID){
+        $sql = "SELECT COUNT(*) FROM account WHERE username = :username";
+        if ($excludeID){
+            $sql .= " and id != :excludeID";
+        }
+
+        $query = $this->db->connect()->prepare($sql);
+        $query->bindParam(':username', $username);
+
+        if ($excludeID){
+            $query->bindParam(':excludeID', $excludeID);
+        }
+
+        $count = $query->execute() ? $query->fetchColumn() : 0;
+
+        return $count > 0;
+    }
+
+    function login($username, $password){
+        $sql = "SELECT * FROM account WHERE username = :username LIMIT 1;";
+        $query = $this->db->connect()->prepare($sql);
+
+        $query->bindParam('username', $username);
+
+        if($query->execute()){
+            $data = $query->fetch();
+            if($data && password_verify($password, $data['password'])){
+                return true;
+            }
+        }
+
+        return false;
+    }
+
+
+    
+    function fetch($username){
+        $sql = "SELECT * FROM account WHERE username = :username LIMIT 1;";
+        $query = $this->db->connect()->prepare($sql);
+
+        $query->bindParam('username', $username);
+        $data = null;
+        if($query->execute()){
+            $data = $query->fetch();
+        }
+
+        return $data;
+    }
+
+    function get_accounts($search='',$role=''){
+        $sql = "SELECT first_name,last_name,role FROM account 
+                    WHERE (first_name LIKE '%' :search '%' OR last_name LIKE '%' :search '%' OR role LIKE '%' :search '%') AND role LIKE '%' :role '%';";
+        $query = $this->db->connect()->prepare($sql);
+
+        $query->bindParam(':search', $search);
+        $query->bindParam(':role', $role);
+        $data = null;
+        if($query->execute()){
+            $data = $query->fetchAll();
+        }
+
+        return $data;
+    }
+
+    function get_role(){
+        $sql = "SELECT DISTINCT(role) FROM account;";
+        $query = $this->db->connect()->prepare($sql);
+
+        $data = null;
+        if($query->execute()){
+            $data = $query->fetchAll();
+        }
+
+        return $data;
+    }
+    
+}
+
+// $obj = new Account();
+
+// $obj->add();
